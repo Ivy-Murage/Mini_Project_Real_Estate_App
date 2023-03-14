1dc1b85ca30d28cb22ae884da9d7946743a211a6
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-14 23:28:08 +0300

    Add form input validation

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,4 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:real_estate_app_mini_project/LoginPage.dart';
@@ -45,12 +47,18 @@ class SignUp extends StatelessWidget {
 }
 
 class FormWidget extends StatefulWidget {
+  // final VoidCallback onClickedSignIn;
+
+  // const FormWidget({Key? key, required this.onClickedSignIn}) : super(key: key);
   @override
   State<FormWidget> createState() => _FormWidgetState();
 }
 
 class _FormWidgetState extends State<FormWidget> {
   bool _isHidden = true;
+
+  final formKey = GlobalKey<FormState>();
+
   final emailController = TextEditingController();
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
@@ -65,10 +73,25 @@ class _FormWidgetState extends State<FormWidget> {
   }
 
   Future SignUp() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
     if (passwordConfirmed()) {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: emailController.text.trim(),
-          password: passwordController.text.trim());
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: emailController.text.trim(),
+            password: passwordController.text.trim());
+      } on FirebaseAuthException catch (e) {
+        print(e);
+      }
+      // Navigator.of(context).pop();
     }
   }
 
@@ -92,121 +115,143 @@ class _FormWidgetState extends State<FormWidget> {
           child: Padding(
             padding: const EdgeInsets.all(10.0),
             child: Form(
+                key: formKey,
                 child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  style: TextStyle(color: Color(0xffF0F0F0)),
-                  controller: emailController,
-                  decoration: InputDecoration(
-                      label: Text(
-                        "Email",
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
                         style: TextStyle(color: Color(0xffF0F0F0)),
+                        controller: emailController,
+                        decoration: InputDecoration(
+                            label: Text(
+                              "Email",
+                              style: TextStyle(color: Color(0xffF0F0F0)),
+                            ),
+                            prefixIcon: Icon(Icons.mark_email_unread_outlined,
+                                color: Color(0xffF0F0F0)),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide:
+                                    BorderSide(color: Color(0xffF0F0F0))),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(
+                                    width: 2.0, color: Color(0xffF0F0F0)))),
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (email) =>
+                            email != null && EmailValidator.validate(email)
+                                ? "Invalid Email"
+                                : null,
                       ),
-                      prefixIcon: Icon(Icons.mark_email_unread_outlined,
-                          color: Color(0xffF0F0F0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Color(0xffF0F0F0))),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                              width: 2.0, color: Color(0xffF0F0F0)))),
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  obscureText: _isHidden,
-                  style: TextStyle(color: Color(0xffF0F0F0)),
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    suffix: InkWell(
-                      onTap: _togglePasswordView,
-                      child: Icon(
-                        _isHidden ? Icons.visibility : Icons.visibility_off,
-                        color: Color(0xffF0F0F0),
-                      ),
-                    ),
-                    label: Text(
-                      "Password",
-                      style: TextStyle(color: Color(0xffF0F0F0)),
-                    ),
-                    prefixIcon: Icon(Icons.lock, color: Color(0xffF0F0F0)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Color(0xffF0F0F0))),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            BorderSide(width: 2.0, color: Color(0xffF0F0F0))),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  obscureText: _isHidden,
-                  style: TextStyle(color: Color(0xffF0F0F0)),
-                  controller: confirmPasswordController,
-                  decoration: InputDecoration(
-                      suffix: InkWell(
-                        onTap: _togglePasswordView,
-                        child: Icon(
-                          _isHidden ? Icons.visibility : Icons.visibility_off,
-                          color: Color(0xffF0F0F0),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        obscureText: _isHidden,
+                        style: TextStyle(color: Color(0xffF0F0F0)),
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          suffix: InkWell(
+                            onTap: _togglePasswordView,
+                            child: Icon(
+                              _isHidden
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Color(0xffF0F0F0),
+                            ),
+                          ),
+                          label: Text(
+                            "Password",
+                            style: TextStyle(color: Color(0xffF0F0F0)),
+                          ),
+                          prefixIcon:
+                              Icon(Icons.lock, color: Color(0xffF0F0F0)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Color(0xffF0F0F0))),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                  width: 2.0, color: Color(0xffF0F0F0))),
                         ),
                       ),
-                      label: Text(
-                        "Password Confirmation",
-                        style: TextStyle(color: Color(0xffF0F0F0)),
+                      SizedBox(
+                        height: 20,
                       ),
-                      prefixIcon: Icon(Icons.lock, color: Color(0xffF0F0F0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Color(0xffF0F0F0))),
-                      iconColor: Color(0xffF0F0F0),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                            width: 2.0,
-                            color: Color(0xffF0F0F0),
-                          ))),
-                ),
-                SizedBox(height: 20),
-                SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xffF0F0F0)),
-                      onPressed: () {
-                        SignUp();
-                      },
-                      child: Text("signup".toUpperCase(),
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold)),
-                    )),
-                Align(
-                  alignment: AlignmentDirectional.center,
-                  child: TextButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return LoginPage();
-                          },
-                        ));
-                      },
-                      child: Text.rich(TextSpan(children: [
-                        TextSpan(text: "Already Have an Account?"),
-                        TextSpan(text: "Login")
-                      ]))),
-                )
-              ],
-            )),
+                      TextFormField(
+                        obscureText: _isHidden,
+                        style: TextStyle(color: Color(0xffF0F0F0)),
+                        controller: confirmPasswordController,
+                        decoration: InputDecoration(
+                            suffix: InkWell(
+                              onTap: _togglePasswordView,
+                              child: Icon(
+                                _isHidden
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Color(0xffF0F0F0),
+                              ),
+                            ),
+                            label: Text(
+                              "Password Confirmation",
+                              style: TextStyle(color: Color(0xffF0F0F0)),
+                            ),
+                            prefixIcon:
+                                Icon(Icons.lock, color: Color(0xffF0F0F0)),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide:
+                                    BorderSide(color: Color(0xffF0F0F0))),
+                            iconColor: Color(0xffF0F0F0),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(
+                                  width: 2.0,
+                                  color: Color(0xffF0F0F0),
+                                ))),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xffF0F0F0)),
+                            onPressed: () {
+                              SignUp();
+                            },
+                            child: Text("signup".toUpperCase(),
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold)),
+                          )),
+                      Align(
+                          alignment: AlignmentDirectional.center,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'No account ',
+                                style: TextStyle(color: Color(0xffF0F0F0)),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => LoginPage()));
+                                },
+                                child: Text(
+                                  'Sign In',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ))
+                    ])),
           )),
     );
   }
@@ -216,4 +261,10 @@ class _FormWidgetState extends State<FormWidget> {
       _isHidden = !_isHidden;
     });
   }
+
+  // void toggle() {
+  //   setState(() {
+  //     _isLogin = !_isLogin;
+  //   });
+  // }
 }
